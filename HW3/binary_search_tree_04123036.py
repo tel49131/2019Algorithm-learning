7ef055fb16c4c4271d0715a1600d2cf2de2ba5a4
tel49131 <44577172+tel49131@users.noreply.github.com>
2019-11-22 22:02:10 +0800

    Add files via upload

---
+++ HW3/binary_search_tree_04123036.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.parent = None
+        """
+        :type val: int
+        :type left: TreeNode or None
+        :type right: TreeNode or None
+        """
+class Solution(object):
+    def insert(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode(inserted node)
+        """
+        if root = None:
+            root = val
+        elif self.left > self.right:
+            self.left = val
+        else self.right = val
+            
+    def delete(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(the root of new completed binary search tree) (cannot search())
+        """
+    def search(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(searched node)
+        """
+    def modify(self, root, target, new_val):
+        """
+        :type root: TreeNode
+        :type target: int
+        :type new_val: int
+        :rtype:TreeNode(the root of new completed binary search tree) (cannot search())
+        """
+        
+
+
+# In[ ]:
+
+
+
+
