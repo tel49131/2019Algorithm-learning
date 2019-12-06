c6ad1dc874ebd6b3db3ea95d954f331f28425282
tel49131 <44577172+tel49131@users.noreply.github.com>
2019-12-06 20:28:47 +0800

    Add files via upload

---
+++ HW4/hash_table_04123036.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        """
+        :type val: int
+        :type next: ListNode
+        :rtype: None        
+        """
+        def get_next(self):
+        return self.next_node
+
+        def get_data(self):
+        return self.data
+        
+class MyHashSet:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.data = [None] * capacity
+        """
+        :type capacity: int
+        :rtype: None
+        """
+        
+    def add(self, key):
+        """
+        :type key: str
+        :rtype: None
+        """
+    def remove(self, key):
+        """
+        :type key: str
+        :rtype: None
+        """
+    def contains(self, key):
+        """
+        :type key: str
+        :rtype: bool(True or False)
+        """
+
