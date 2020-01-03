c56015ee9cc633285a3ba3d9208229f1329c223c
tel49131 <44577172+tel49131@users.noreply.github.com>
2020-01-03 19:58:27 +0800

    Add files via upload

---
+++ HW6/Dijkstra_04123036.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+from collections import defaultdict 
+
+
+class Graph(): 
+
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [] 
+        self.graph_matrix = [[0 for column in range(vertices)]  
+                    for row in range(vertices)] 
+    def addEdge(self,u,v,w): 
+         
+    def Dijkstra(self, s): 
+
+    def Kruskal(self):
+
